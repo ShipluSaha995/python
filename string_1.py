a665892e7bc4b4e1ff0fa9e09cd4fb31a8b498cd
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-05 18:25:16 +0600

    learning

--- string_1.py
+++ string_1.py
@@ -1,1 +1,7 @@
 print("hello world")
+massage='hello'
+print(massage)
+print("length of the str=", len(massage)) #len used to find the length of the string
+print(massage[4]) #finds the 4th index of the string 
+print(massage[0:3])#prints the 1st to 3rd element from the string
+print(massage[:3])# same like the previous one  
