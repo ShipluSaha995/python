e489b20ce5f7e902f24968812853793c12f849a2
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-06 19:51:13 +0600

    updated

---
+++ Conditional_Operator.py
@@ -0,0 +1,9 @@
+is_hot=input()
+is_cold=input()
+
+if is_hot:
+    print("It's a hot day. \nDrink Placty of water")
+elif is_cold:
+    print("It's a cold day\nWear warm colths.")
+else:
+    print("It's a lovely day.")
--- practice_1.py
+++ practice_1.py
@@ -6,7 +6,7 @@ color=input("What is your fovourite color?\n")
 print(name +" likes "+color+"\n")
 """
 # Type conversition
-
+"""
 birth_year=int(input("Your birth year: "))
 Running_year=int(input("Running Year: "))
 age=Running_year - birth_year
@@ -16,5 +16,18 @@ print("Your age is: ", age)
 weight=float(input("Enter your weight(lbs): "))
 convert=float(weight*0.453592)
 print("Your weight in kilogram is: ", convert)
+
+"""
+
+#conditional operator
+price=float(input())
+good_credit=input()
+
+if good_credit:
+    down_payment=price*0.1
+else:
+    down_payment=price*0.2
+
+print(f"down payment:${down_payment}")
 
 
