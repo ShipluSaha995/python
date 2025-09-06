42624d2b81cf91e02fe3115d1b053c04d381ed18
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-06 18:34:58 +0600

    updated

---
+++ Arithmatic_Operator.py
@@ -0,0 +1,35 @@
+#Arithmetic Operators
+
+"""
+Addition -> +
+substractio -> -
+multiplicatio -> *
+division -> /
+Modulus -> %
+Exponent -> **
+Floor Division -> //
+
+"""
+"""
+num1=int(input())
+num2=int(input())
+
+sum=num1+num2
+sub=(num1-num2)
+div=(num1/num2)
+mul=(num1*num2)
+exp=(num1**num2)
+fl=num1//num2
+
+num1+=10
+
+print(sum, sub, mul, div, exp, fl, num1,"\n")
+print((3+2)*5-1)
+
+print(round(2.5))
+print(abs(-2.5))
+
+"""
+
+import math
+print(math.ceil(2.9))
