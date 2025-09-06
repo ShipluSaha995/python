a9f232ae0d09f212c2a0cda65e161c2751301a15
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-06 11:54:05 +0600

    updated

--- practice_1.py
+++ practice_1.py
@@ -1,7 +1,12 @@
 #take user input  name and his favourite color and print them
 
-name=input("What is your name?\n")
-print("Hi "+name)
-color=input("What is your fovourite color?\n")
-print(name +" likes "+color+"\n")
+#name=input("What is your name?\n")
+#print("Hi "+name)
+#color=input("What is your fovourite color?\n")
+#print(name +" likes "+color+"\n")
 
+# Type conversition
+birth_year=int(input("Your birth year: "))
+Running_year=int(input("Running Year: "))
+age=Running_year - birth_year
+print("Your age is: ", age)
