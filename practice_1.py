5c847ba7a85772ae6092499e005a0b8e4afc9ce2
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-06 11:14:32 +0600

    updated

---
+++ practice_1.py
@@ -0,0 +1,6 @@
+#take user input  name and his favourite color and print your favourite color
+
+name=input("What is your name?\n")
+print("Hi "+name)
+color=input("What is your fovourite color?\n")
+print("Shiplu likes blue.\n")
