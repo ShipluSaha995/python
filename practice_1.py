1d6f60fe272f846a6098dc8ef9bc5370dea40263
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-06 13:39:19 +0600

    new file added

--- practice_1.py
+++ practice_1.py
@@ -1,17 +1,20 @@
 #take user input  name and his favourite color and print them
-
-#name=input("What is your name?\n")
-#print("Hi "+name)
-#color=input("What is your fovourite color?\n")
-#print(name +" likes "+color+"\n")
-
+"""
+name=input("What is your name?\n")
+print("Hi "+name)
+color=input("What is your fovourite color?\n")
+print(name +" likes "+color+"\n")
+"""
 # Type conversition
+
 birth_year=int(input("Your birth year: "))
 Running_year=int(input("Running Year: "))
 age=Running_year - birth_year
 print("Your age is: ", age)
 
 
-weight=float(input("Enter your weight(pound): "))
+weight=float(input("Enter your weight(lbs): "))
 convert=float(weight*0.453592)
 print("Your weight in kilogram is: ", convert)
+
+
