e6bdcaffc70accb0aac6a6835098411c68368d33
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-07 16:45:51 +0600

    updated

--- loop.py
+++ loop.py
@@ -1,6 +1,16 @@
 #While loop
-
+"""
 i=1
 while i<=5:
     print(' *'*i)
     i+=1
+"""
+
+string=input("\ninput the string that you want to print: ")
+i=1
+end=int(input("\nHow many times do you want to print : "))
+
+while(i<=end):
+    print(f"{i}. {string}")
+    i+=1
+    
