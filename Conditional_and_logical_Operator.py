8a5d4189abcbcf702962058770772e407aa15d11
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-07 08:13:45 +0600

    updated

--- Conditional_and_logical_Operator.py
+++ Conditional_and_logical_Operator.py
@@ -13,4 +13,26 @@ else:
 
 """
 
-#kogical operator
+#kogical operator( and, or )
+#and= both conditions has to be true
+#or= one condition has to be true
+#not = change the boolien from true to false or false to true
+
+"""
+high_income=input()
+good_credit=input()
+
+
+if high_income=="True"  or good_credit=="True" :
+    print("Eligible for loan.\n")
+else:
+    print("Not eligibl.\n")
+"""
+
+good_credit= input()
+criminal_records=input()
+
+if good_credit=="True" and not criminal_records=="True":
+    print("Eligible for Loan\n")
+else:
+    print("Not eligible for loan.\n")
