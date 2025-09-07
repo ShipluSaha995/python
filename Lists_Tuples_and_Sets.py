392083280a5ed0e949eef293e8406b32840c2fff
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-07 12:47:19 +0600

    Updated

--- Lists_Tuples_and_Sets.py
+++ Lists_Tuples_and_Sets.py
@@ -1,11 +1,21 @@
-                                    
+#lists
+course=['History', 'Physics', 'Math', 'CSE', 'ML']
+print(course,"\n")
+print(course[0:2], "\n")
+print(course[2:], "\n")
 
+course.insert(0, 'AI') #this inserrt function is use to add an element in a requiered index 
+print(course, "\n")
 
+course_2=['CSE 205', 'CSE 206']
+course.extend(course_2)  #this extend function used to concat 2 lists  
+print(course, "\n")
 
+course.append(course_2) # append is use to add a list in a list 
+print(course, "\n") 
 
+course.remove("History")
+print(course)
 
-
-
-
-
-                                            """ This file is empty for now """
+course.pop()
+print(course, "\n")
