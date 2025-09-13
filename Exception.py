8e1dcf423ec586782c1595d80d14c00dfca3914b
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-13 11:19:46 +0600

    Updated

--- Exception.py
+++ Exception.py
@@ -1,8 +1,21 @@
 #Error handeling
-
+"""
 try:
     age=int(input("Age: "))
     print(age)
 except ValueError:
     print("Invalid value\n")
 
+"""
+
+try:
+    age=int(input("Age:" ))
+    income=1000000
+    #risk=income/0 ## it will give zero division error
+    risk=income/age
+    print(age)
+    print(risk)
+except ZeroDivisionError:
+    print("Age can not be zero.\n")
+except ValueError:
+    print("Error\n")
