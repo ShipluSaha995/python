b883556ce419aea3e7d11de97b0ebb2b07a1b7f5
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-14 11:51:06 +0600

    Updated

---
+++ classes.py
@@ -0,0 +1,13 @@
+# class EmalClient # in class we have to capitalize the first letter of every letter.
+
+class Point:
+    def move(self):
+        print("move\n")
+    def draw(self):
+        print("draw")
+    
+point1=Point()
+point1.draw()
+point1.x=10
+point1.x=20
+print(point1.x)
