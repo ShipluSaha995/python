604cc7d82c27f08798ea9134ffa79e739aa37b60
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-17 22:18:06 +0600

    updated

--- pacakge.py
+++ pacakge.py
@@ -4,7 +4,13 @@ ecomerce.shipping.calc_shipping()
 """
 
 #we can also do this like
+"""
 from ecomerce.shipping import calc_shipping
 calc_shipping()
 calc_shipping()
 calc_shipping()
+"""
+
+#also we can
+from ecomerce import shipping
+shipping.calc_shipping()
