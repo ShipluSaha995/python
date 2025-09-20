637d87bbd3a43eabd2713811725a1bd172c07dc2
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-20 11:41:36 +0600

    updated

--- 1_st_project/Excel_spread_sheet.py
+++ 1_st_project/Excel_spread_sheet.py
@@ -1,5 +1,5 @@
 import openpyxl as xl
-
+from openpyxl.chart import BarChart, Reference
 # Load workbook from full path
 wb = xl.load_workbook("Transaction.xlsx")
 sheet = wb['Sheet1']
@@ -22,6 +22,16 @@ for row in range(2, sheet.max_row + 1):
     corrected_price=cell.value*0.9
     corrected_price_cell=sheet.cell(row, 4)
     corrected_price_cell.value=corrected_price
+
+values=Reference(sheet, 
+                 min_row=2, 
+                 max_row=sheet.max_row,
+                 min_col=4,
+                 max_col=4)
+
+chart=BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'e2')
 
 # Save back to same file
 wb.save("Transaction.xlsx")
