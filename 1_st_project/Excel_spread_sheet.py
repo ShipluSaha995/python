a6810ba8b771c24601b1d9e896132724f69a0cc0
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-20 11:49:52 +0600

    Updated

--- 1_st_project/Excel_spread_sheet.py
+++ 1_st_project/Excel_spread_sheet.py
@@ -1,37 +1,42 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 # Load workbook from full path
-wb = xl.load_workbook("Transaction.xlsx")
-sheet = wb['Sheet1']
 
-# Print row numbers
-for row in range(1, sheet.max_row + 1):
-    print(row)
+def proccess_workbook(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
 
-# Print values
-for row in range(1, sheet.max_row + 1):
-    cell1 = sheet.cell(row, 1)
-    cell2 = sheet.cell(row, 2)
-    cell3 = sheet.cell(row, 3)
-    cell4 = sheet.cell(row, 4)
-    print(cell1.value, " ", cell2.value, " ", cell3.value, " ", cell4.value)
+    # Print row numbers
+    for row in range(1, sheet.max_row + 1):
+        print(row)
 
-# Update price
-for row in range(2, sheet.max_row + 1):
-    cell = sheet.cell(row, 3)
-    corrected_price=cell.value*0.9
-    corrected_price_cell=sheet.cell(row, 4)
-    corrected_price_cell.value=corrected_price
+    # Print values
+    for row in range(1, sheet.max_row + 1):
+        cell1 = sheet.cell(row, 1)
+        cell2 = sheet.cell(row, 2)
+        cell3 = sheet.cell(row, 3)
+        cell4 = sheet.cell(row, 4)
+        print(cell1.value, " ", cell2.value, " ", cell3.value, " ", cell4.value)
 
-values=Reference(sheet, 
-                 min_row=2, 
-                 max_row=sheet.max_row,
-                 min_col=4,
-                 max_col=4)
+    # Update price
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price=cell.value*0.9
+        corrected_price_cell=sheet.cell(row, 4)
+        corrected_price_cell.value=corrected_price
 
-chart=BarChart()
-chart.add_data(values)
-sheet.add_chart(chart, 'e2')
+    values=Reference(sheet, 
+                    min_row=2, 
+                    max_row=sheet.max_row,
+                    min_col=4,
+                    max_col=4)
 
-# Save back to same file
-wb.save("Transaction.xlsx")
+    chart=BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+
+    # Save back to same file
+    wb.save(filename)
+
+
+#After run this type ".\Transaction.xlsx" in th terminal
