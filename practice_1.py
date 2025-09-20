b2e7c0325f2cdb1bf352068b900dedfa79b94550
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-20 16:56:06 +0600

    Updated

--- practice_1.py
+++ practice_1.py
@@ -242,7 +242,7 @@ def find_max(numbers):
             maximum=i
     return maximum
 """
-
+"""
 ##Dice roll
 import random
 class Dice:
@@ -253,3 +253,35 @@ class Dice:
 
 dice=Dice()
 print(dice.roll())
+"""
+
+#simple calculator
+print("\n\t\t\t Normal Calculator\n\t\t\t____________________\n\n")
+
+print("1. Addition.\n2. Substraction.\n3. Multiplication.\n4. Division\n")
+choice=int(input("Enter your choice: "))
+
+if choice not in [1,2,3,4]:
+    print("Invalid Choice")
+
+else:
+    
+    num1=float(input("Enter first number: "))
+    num2=float(input("Enter second number: "))
+
+    if choice==1:
+        print(f"Result: {num1+num2}")
+
+    elif choice==2:
+        print(f"Result: {num1-num2}")
+
+    elif choice==3:
+        print(f"Result: {num1*num2}")
+
+    elif choice==4:
+        if num2!=0:
+            print(f"Result: {num1/num2}")
+
+        else :
+            print("can not divided by zero it will bw an infinity.\n")
+
