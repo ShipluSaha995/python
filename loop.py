ec76d04f3dc9b4af86722d86594f5f7492b14a4f
ShipluSaha995 <shiplusaha995@gmail.com>
2025-09-22 14:10:01 +0600

    Updated

--- loop.py
+++ loop.py
@@ -49,9 +49,15 @@ for i in numbers:
 """
 
 #break statement
-
+"""
 for i in range(12):
      if i==10:
         break
      print('5 X', i+1, "=", 5*(i+1))
-   
+"""
+
+#continue
+for i in range(1, 6):
+    if i % 2 == 0:
+        continue  # skip even numbers
+    print(i)
