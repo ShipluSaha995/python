501a512bba29b9ce53072af7d461519cd4c84b20
ShipluSaha995 <shiplusaha995@gmail.com>
2025-11-15 18:13:07 +0600

    importing modules

--- import_modulus.py
+++ import_modulus.py
@@ -1,5 +1,14 @@
+'''
 import my_moduls as mm
 
 course=["phy", "math", "computer Sci"]
 index= mm.find_index(course, "math")
+print(index)
+'''
+
+#we can also do
+
+from my_moduls import find_index
+course=["phy", "math", "computer Sci"]
+index= find_index(course, "math")
 print(index)
